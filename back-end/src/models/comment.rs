d3b28dd542361f9c4bb3534dd05f307edc410997
Alex Howard <alex.howard@powershades.com>
2023-12-22 21:45:32 -0600

    fixing struct name

--- back-end/src/models/comment.rs
+++ back-end/src/models/comment.rs
@@ -4,7 +4,7 @@ use crate::schema::comment;
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = comment)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct User {
+pub struct Comment {
 	pub id: Option<i32>,
 	pub user_id: i32,
 	pub details: String,
