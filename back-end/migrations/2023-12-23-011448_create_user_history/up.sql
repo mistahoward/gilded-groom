efc2de2d8c3c00862f9df519fb615c01f8d55efa
Alex Howard <alex.howard@powershades.com>
2023-12-22 21:12:15 -0600

    adding user foreign key constraint i forgot to add

--- back-end/migrations/2023-12-23-011448_create_user_history/up.sql
+++ back-end/migrations/2023-12-23-011448_create_user_history/up.sql
@@ -10,7 +10,7 @@ INSERT INTO operation (name, description) VALUES ('DELETE', 'Delete an existing 
 
 CREATE TABLE user_history (
 	id INTEGER PRIMARY KEY,
-	user_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL REFERENCES user(id),
 	field_name TEXT NOT NULL,
 	old_value TEXT,
 	new_value TEXT,
